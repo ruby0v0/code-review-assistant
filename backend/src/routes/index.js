2a687546189ad222c8dba104955cc2a031bd2c63
鲨鱼辣椒 <monishine_code@163.com>
2025-12-27 12:51:41 +0800

    refactor: 路由聚合

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,3 +1,10 @@
+import { Router } from 'express'
 import reviewRoutes from './review.routes.js';
+import repositoryRoutes from './repository.routes.js';
 
-export { reviewRoutes };
+const router = Router();
+
+router.use('/review', reviewRoutes);
+router.use('/repository', repositoryRoutes);
+
+export default router;
